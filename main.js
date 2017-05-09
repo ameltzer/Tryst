5c5e80fc5a5cdf106765f78a55da93eab8602372
Aaron <ameltzer91@gmail.com>
2017-05-08 22:31:55 -0400

    REST framework

--- main.js
+++ main.js
@@ -1,17 +1,48 @@
 var express = require('express');
 var app = express();
-credentials = {
-      access_token: "-CrAvAUl3e-8aRySvQDkRIHv_29VfQH-ZdOximEzkX8GTMAnMDcAy_GZZlBe5RjHoMn7M0xhHfF-0KJN44MJ9fOwDFCa-cV1tIPax22XgTzu2kbhpztrR2JiupkOWXYx"
-};
-var rest = require('LightYelpWrapper').create(credentials);
+
+const REST = require('RESTFULService');
+function buildREST(RESTData){
+    var restService = REST.create(RESTData.baseUrl,RESTData.accessToken);
+    for(var endpoint in RESTData.endPoints){
+        restService.registerEndpoint(endpoint.endpoint, endpoint.funcName);
+    }
+    return restService;
+}
+
+yelp_credentials_access_token= "-CrAvAUl3e-8aRySvQDkRIHv_29VfQH-ZdOximEzkX8GTMAnMDcAy_GZZlBe5RjHoMn7M0xhHfF-0KJN44MJ9fOwDFCa-cV1tIPax22XgTzu2kbhpztrR2JiupkOWXYx"
+var yelpREST = require('yelp-fusion').client(yelp_credentials_access_token);
 app.get('/', function(req,res){
     var options = {
         term:'Four Barrel Coffee',
         location:'san francisco, ca'
     };
-    rest.call(options,function(data){
+    yelpREST.search(options).then(function(data){
        console.log(data); 
        res.send(data);
     });
 });
+
+
+googleRESTData = {
+    baseUrl: '',
+    accessToken: '',
+    endPoints: [
+        { endpoint:'/place/nearbysearch/output', funcName:'placeSearch'},
+        { endpoint:'/a/b/c/output', funcName:'abc'}
+    ]
+};
+var googleREST = buildREST(googleRESTData);
+
+
+app.get('/map', function(req,res){
+   var options = {
+        term:'Four Barrel Coffee',
+        location:'san francisco, ca'
+   };
+   googleREST.placeSearch(options).then(function(data){
+        console.log('yay');
+   });
+});
+
 app.listen(3000);
